e3b49af1781c096ecffae494041b50ff90c5adae
anniori <77315551+anniori@users.noreply.github.com>
2021-09-15 13:29:51 +0800

    Add files via upload

--- js/kirajs.js
+++ js/kirajs.js
@@ -8,3 +8,21 @@ $(document).ready(function () {
 });
 
 
+/*--------------gotop-------------------*/
+$(document).ready(function(){
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('#goTop').fadeIn();
+		} else {
+			$('#goTop').fadeOut();
+		}
+	});
+	//Click event to scroll to top
+	$('#goTop').click(function(){
+		$('html, body').animate({scrollTop : 0},500);
+		return false;
+	});
+});
+
+
