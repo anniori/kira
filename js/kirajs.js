c19f9ec07e28aed09b43c9ad11def9193b8f70f1
anniori <77315551+anniori@users.noreply.github.com>
2021-09-16 15:58:34 +0800

    Add files via upload

--- js/kirajs.js
+++ js/kirajs.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
  
-    /* $('.dropdown').click(function (event) { 
+    $('.dropdown').click(function (event) { 
         event.preventDefault();
          $('.dropdown').toggleClass('active');
         $('.dropdown-open').slideToggle();
-    }); */
+    });
 });
 
 
@@ -20,7 +20,7 @@ $(document).ready(function(){
 	});
 	//Click event to scroll to top
 	$('#goTop').click(function(){
-		$('html, body').animate({scrollTop : 0},200);
+		$('html, body').animate({scrollTop : 0},1000);
 		return false;
 	});
 });
